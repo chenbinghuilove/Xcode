0355969ec4033a515508fbb7a021118504cde047
chen <chen@appledeMacBook-Air.local>
2015-05-27 15:51:27 +0800

    test

---
+++ test/test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by chen on 15/5/27.
+//  Copyright (c) 2015年 chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"chenbinghuilove");
+    }
+    return 0;
+}
